88fa17309f765c3610c216ed3baa4b75523e4e5f
Roman Razhnyi <rapax@alarstudios.com>
2022-02-25 16:03:50 +0300

    fix

--- src/components/HappifierTable.jsx
+++ src/components/HappifierTable.jsx
@@ -1,20 +1,10 @@
+import { HappifierRow } from "./HappifierRow";
 
 export const HappifierTable = ({happifier}) => {
   return (
     <table>
       <tbody>
-        {happifier.map((field, index) => {
-          return (
-            <tr key={index}>
-              <td>
-                <label for={field.id}>{field.title}</label>
-              </td>
-              <td>
-                <input id={field.id} placeholder={field.placeholder} value={field.value}/>
-              </td>
-            </tr>
-          )
-        })}
+        {happifier.map((field, index) => <HappifierRow key={index} field={field}/>)}
       </tbody>
     </table>
   );

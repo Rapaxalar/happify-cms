75f795fea2f15c67c4c816e31f1fc0d732438766
Roman Razhnyi <rapax@alarstudios.com>
2022-02-25 15:55:42 +0300

    commit

--- src/api/index.js
+++ src/api/index.js
@@ -1,316 +1,61 @@
 
+const fields = [
+  'happifier_type',
+  'human_url',
+  'created_at',
+  'modified_at',
+  'video_url_translations',
+  'is_promoted',
+  'id_featured',
+  'author_translations',
+  'title_translations',
+  'subtitle_translations',
+  'meta_title_translations',
+  'meta_description_translations',
+  'image_translations',
+  'right_rail_image_translations',
+  'image_alt_translations',
+  'body_translations',
+  'credits_translations',
+  'og_title_translations',
+  'og_description_translations',
+  'og_image_translations',
+  'publish_at_translations',
+  'is_enabled',
+  'view_count',
+  'viral_coeff',
+  'promo_factor',
+  'serenity_scene',
+  'serenity_guide',
+  'serenity_time',
+  'hog_scene',
+  'available_locales',
+  'generic_helped_association_id',
+  'generic_comment_association_id',
+  'generic_like_association_id',
+  'is_sponsored',
+  'author_image_translations',
+  'author_info_translations',
+  'sponsor',
+  'audio_url_translations',
+  'publisher_id',
+  'original_image_translations',
+  'image_click_url_translations',
+  'image_id',
+  'right_rail_image_id',
+  'og_image_id',
+]
 
 export const getHappifier = ({id}) => {
-  return [
-    {
-      id: 'happifier_type',
+  return fields.map(field => {
+    return {
+      id: field,
       type: 'string',
-      title: `Title of happifier_type`,
-      value: `happifier_type ${id}`,
-      placeholder: `Please enter happifier_type`
-    },
-    {
-      id: 'human_url',
-      type: 'string',
-      title: `Title of human_url`,
-      value: `human_url ${id}`,
-      placeholder: `Please enter human_url`
-    },
-    {
-      id: 'created_at',
-      type: 'string',
-      title: `Title of created_at`,
-      value: `created_at ${id}`,
-      placeholder: `Please enter created_at`
-    },
-    {
-      id: 'modified_at',
-      type: 'string',
-      title: `Title of modified_at`,
-      value: `modified_at ${id}`,
-      placeholder: `Please enter modified_at`
-    },
-    {
-      id: 'video_url_translations',
-      type: 'string',
-      title: `Title of video_url_translations`,
-      value: `video_url_translations ${id}`,
-      placeholder: `Please enter video_url_translations`
-    },
-    {
-      id: 'is_promoted',
-      type: 'string',
-      title: `Title of is_promoted`,
-      value: `is_promoted ${id}`,
-      placeholder: `Please enter is_promoted`
-    },
-    {
-      id: 'id_featured',
-      type: 'string',
-      title: `Title of id_featured`,
-      value: `id_featured ${id}`,
-      placeholder: `Please enter id_featured`
-    },
-    {
-      id: 'author_translations',
-      type: 'string',
-      title: `Title of author_translations`,
-      value: `author_translations ${id}`,
-      placeholder: `Please enter author_translations`
-    },
-    {
-      id: 'title_translations',
-      type: 'string',
-      title: `Title of title_translations`,
-      value: `title_translations ${id}`,
-      placeholder: `Please enter title_translations`
-    },
-    {
-      id: 'subtitle_translations',
-      type: 'string',
-      title: `Title of subtitle_translations`,
-      value: `subtitle_translations ${id}`,
-      placeholder: `Please enter subtitle_translations`
-    },
-    {
-      id: 'meta_title_translations',
-      type: 'string',
-      title: `Title of meta_title_translations`,
-      value: `meta_title_translations ${id}`,
-      placeholder: `Please enter meta_title_translations`
-    },
-    {
-      id: 'meta_description_translations',
-      type: 'string',
-      title: `Title of meta_description_translations`,
-      value: `meta_description_translations ${id}`,
-      placeholder: `Please enter meta_description_translations`
-    },
-    {
-      id: 'image_translations',
-      type: 'string',
-      title: `Title of image_translations`,
-      value: `image_translations ${id}`,
-      placeholder: `Please enter image_translations`
-    },
-    {
-      id: 'right_rail_image_translations',
-      type: 'string',
-      title: `Title of right_rail_image_translations`,
-      value: `right_rail_image_translations ${id}`,
-      placeholder: `Please enter right_rail_image_translations`
-    },
-    {
-      id: 'image_alt_translations',
-      type: 'string',
-      title: `Title of image_alt_translations`,
-      value: `image_alt_translations ${id}`,
-      placeholder: `Please enter image_alt_translations`
-    },
-    {
-      id: 'body_translations',
-      type: 'string',
-      title: `Title of body_translations`,
-      value: `body_translations ${id}`,
-      placeholder: `Please enter body_translations`
-    },
-    {
-      id: 'credits_translations',
-      type: 'string',
-      title: `Title of credits_translations`,
-      value: `credits_translations ${id}`,
-      placeholder: `Please enter credits_translations`
-    },
-    {
-      id: 'og_title_translations',
-      type: 'string',
-      title: `Title of og_title_translations`,
-      value: `og_title_translations ${id}`,
-      placeholder: `Please enter og_title_translations`
-    },
-    {
-      id: 'og_description_translations',
-      type: 'string',
-      title: `Title of og_description_translations`,
-      value: `og_description_translations ${id}`,
-      placeholder: `Please enter og_description_translations`
-    },
-    {
-      id: 'og_image_translations',
-      type: 'string',
-      title: `Title of og_image_translations`,
-      value: `og_image_translations ${id}`,
-      placeholder: `Please enter og_image_translations`
-    },
-    {
-      id: 'publish_at_translations',
-      type: 'string',
-      title: `Title of publish_at_translations`,
-      value: `publish_at_translations ${id}`,
-      placeholder: `Please enter publish_at_translations`
-    },
-    {
-      id: 'is_enabled',
-      type: 'string',
-      title: `Title of is_enabled`,
-      value: `is_enabled ${id}`,
-      placeholder: `Please enter is_enabled`
-    },
-    {
-      id: 'view_count',
-      type: 'string',
-      title: `Title of view_count`,
-      value: `view_count ${id}`,
-      placeholder: `Please enter view_count`
-    },
-    {
-      id: 'viral_coeff',
-      type: 'string',
-      title: `Title of viral_coeff`,
-      value: `viral_coeff ${id}`,
-      placeholder: `Please enter viral_coeff`
-    },
-    {
-      id: 'promo_factor',
-      type: 'string',
-      title: `Title of promo_factor`,
-      value: `promo_factor ${id}`,
-      placeholder: `Please enter promo_factor`
-    },
-    {
-      id: 'serenity_scene',
-      type: 'string',
-      title: `Title of serenity_scene`,
-      value: `serenity_scene ${id}`,
-      placeholder: `Please enter serenity_scene`
-    },
-    {
-      id: 'serenity_guide',
-      type: 'string',
-      title: `Title of serenity_guide`,
-      value: `serenity_guide ${id}`,
-      placeholder: `Please enter serenity_guide`
-    },
-    {
-      id: 'serenity_time',
-      type: 'string',
-      title: `Title of serenity_time`,
-      value: `serenity_time ${id}`,
-      placeholder: `Please enter serenity_time`
-    },
-    {
-      id: 'hog_scene',
-      type: 'string',
-      title: `Title of hog_scene`,
-      value: `hog_scene ${id}`,
-      placeholder: `Please enter hog_scene`
-    },
-    {
-      id: 'available_locales',
-      type: 'string',
-      title: `Title of available_locales`,
-      value: `available_locales ${id}`,
-      placeholder: `Please enter available_locales`
-    },
-    {
-      id: 'generic_helped_association_id',
-      type: 'string',
-      title: `Title of generic_helped_association_id`,
-      value: `generic_helped_association_id ${id}`,
-      placeholder: `Please enter generic_helped_association_id`
-    },
-    {
-      id: 'generic_comment_association_id',
-      type: 'string',
-      title: `Title of generic_comment_association_id`,
-      value: `generic_comment_association_id ${id}`,
-      placeholder: `Please enter generic_comment_association_id`
-    },
-    {
-      id: 'generic_like_association_id',
-      type: 'string',
-      title: `Title of generic_like_association_id`,
-      value: `generic_like_association_id ${id}`,
-      placeholder: `Please enter generic_like_association_id`
-    },
-    {
-      id: 'is_sponsored',
-      type: 'string',
-      title: `Title of is_sponsored`,
-      value: `is_sponsored ${id}`,
-      placeholder: `Please enter is_sponsored`
-    },
-    {
-      id: 'author_image_translations',
-      type: 'string',
-      title: `Title of author_image_translations`,
-      value: `author_image_translations ${id}`,
-      placeholder: `Please enter author_image_translations`
-    },
-    {
-      id: 'author_info_translations',
-      type: 'string',
-      title: `Title of author_info_translations`,
-      value: `author_info_translations ${id}`,
-      placeholder: `Please enter author_info_translations`
-    },
-    {
-      id: 'sponsor',
-      type: 'string',
-      title: `Title of sponsor`,
-      value: `sponsor ${id}`,
-      placeholder: `Please enter sponsor`
-    },
-    {
-      id: 'audio_url_translations',
-      type: 'string',
-      title: `Title of audio_url_translations`,
-      value: `audio_url_translations ${id}`,
-      placeholder: `Please enter audio_url_translations`
-    },
-    {
-      id: 'publisher_id',
-      type: 'string',
-      title: `Title of publisher_id`,
-      value: `publisher_id ${id}`,
-      placeholder: `Please enter publisher_id`
-    },
-    {
-      id: 'original_image_translations',
-      type: 'string',
-      title: `Title of original_image_translations`,
-      value: `original_image_translations ${id}`,
-      placeholder: `Please enter original_image_translations`
-    },
-    {
-      id: 'image_click_url_translations',
-      type: 'string',
-      title: `Title of image_click_url_translations`,
-      value: `image_click_url_translations ${id}`,
-      placeholder: `Please enter image_click_url_translations`
-    },
-    {
-      id: 'image_id',
-      type: 'string',
-      title: `Title of image_id`,
-      value: `image_id ${id}`,
-      placeholder: `Please enter image_id`
-    },
-    {
-      id: 'right_rail_image_id',
-      type: 'string',
-      title: `Title of right_rail_image_id`,
-      value: `right_rail_image_id ${id}`,
-      placeholder: `Please enter right_rail_image_id`
-    },
-    {
-      id: 'og_image_id',
-      type: 'string',
-      title: `Title of og_image_id`,
-      value: `og_image_id ${id}`,
-      placeholder: `Please enter og_image_id`
-    },
-  ]
+      title: `Title of ${field}`,
+      value: `Value of ${field} ${id}`,
+      placeholder: `Placeholder of ${field}`
+    }
+  })
 }
 
 export const getHappifiersList = () => {
